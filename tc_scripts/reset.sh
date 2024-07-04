eee01cb96bc620d146b54d423ec5a8672f70217b
lasse <lasse-winkel@t-online.de>
2024-07-04 17:32:10 +0200

    Extend tc scripts for third namespace

--- tc_scripts/reset.sh
+++ tc_scripts/reset.sh
@@ -4,6 +4,8 @@ if [ "$NETWORK_NAMESPACE" = "ns-rs" ]; then
     INTERFACE="app-rs"
 elif [ "$NETWORK_NAMESPACE" = "ns-js" ]; then
     INTERFACE="app-js"
+elif [ "$NETWORK_NAMESPACE" = "ns-js-sub" ]; then
+    INTERFACE="app-rs-sub"
 fi
 
 if sudo ip netns exec $NETWORK_NAMESPACE tc qdisc show dev $INTERFACE | grep netem; then
--- tc_scripts/throttle.sh
+++ tc_scripts/throttle.sh
@@ -7,6 +7,8 @@ if [ "$NETWORK_NAMESPACE" = "ns-rs" ]; then
     INTERFACE="app-rs"
 elif [ "$NETWORK_NAMESPACE" = "ns-js" ]; then
     INTERFACE="app-js"
+elif [ "$NETWORK_NAMESPACE" = "ns-js-sub" ]; then
+    INTERFACE="app-rs-sub"
 fi
 
 # Display the configuration
