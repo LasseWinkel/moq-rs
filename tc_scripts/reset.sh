9a7e277b383bd040a84e2f2b545b6931d75a0dc1
lasse <lasse-winkel@t-online.de>
2024-08-20 20:34:38 +0200

    Add config file for manual network configurations

--- tc_scripts/reset.sh
+++ tc_scripts/reset.sh
@@ -1,17 +1,39 @@
+# Path to the JSON file
+config_file="../config.json"
+
+# Read values from JSON file using jq
+NETWORK_INTERFACE_PUBLISHER=$(jq -r '.networkInterfacePublisher' "$config_file")
+NETWORK_INTERFACE_SERVER=$(jq -r '.networkInterfaceServer' "$config_file")
+NETWORK_INTERFACE_REMOTE_SUBSCRIBER=$(jq -r '.networkInterfaceRemoteSubscriber' "$config_file")
+ARE_LINUX_NETWORK_NAMESPACES_USED=$(jq -r '.areLinuxNetworkNamespacesUsed' "$config_file")
+
+
 NETWORK_NAMESPACE=$1
 
 if [ "$NETWORK_NAMESPACE" = "ns-rs" ]; then
-    INTERFACE="app-rs"
+    INTERFACE=$NETWORK_INTERFACE_SERVER
 elif [ "$NETWORK_NAMESPACE" = "ns-js" ]; then
-    INTERFACE="app-js"
+    INTERFACE=$NETWORK_INTERFACE_PUBLISHER
 elif [ "$NETWORK_NAMESPACE" = "ns-js-sub" ]; then
 	NETWORK_NAMESPACE="ns-rs"
-    INTERFACE="app-rs-sub"
+    INTERFACE=$NETWORK_INTERFACE_REMOTE_SUBSCRIBER
 fi
 
-if sudo ip netns exec $NETWORK_NAMESPACE tc qdisc show dev $INTERFACE | grep netem; then
-    echo "ip netns exec $NETWORK_NAMESPACE tc qdisc del dev $INTERFACE root"
-    sudo ip netns exec $NETWORK_NAMESPACE tc qdisc del dev $INTERFACE root
+if [ $ARE_LINUX_NETWORK_NAMESPACES_USED = true ]; then
+	if sudo ip netns exec $NETWORK_NAMESPACE tc qdisc show dev $INTERFACE | grep netem; then
+		echo "ip netns exec $NETWORK_NAMESPACE tc qdisc del dev $INTERFACE root"
+		sudo ip netns exec $NETWORK_NAMESPACE tc qdisc del dev $INTERFACE root
+	else
+		echo "no netem rule"
+	fi
 else
-    echo "no netem rule"
+	if sudo tc qdisc show dev $INTERFACE | grep netem; then
+		echo "tc qdisc del dev $INTERFACE root"
+		sudo tc qdisc del dev $INTERFACE root
+	else
+		echo "no netem rule"
+	fi
 fi
+
+
+
--- tc_scripts/throttle.sh
+++ tc_scripts/throttle.sh
@@ -1,20 +1,36 @@
+# Path to the JSON file
+config_file="../config.json"
+
+# Read values from JSON file using jq
+NETWORK_INTERFACE_PUBLISHER=$(jq -r '.networkInterfacePublisher' "$config_file")
+NETWORK_INTERFACE_SERVER=$(jq -r '.networkInterfaceServer' "$config_file")
+NETWORK_INTERFACE_REMOTE_SUBSCRIBER=$(jq -r '.networkInterfaceRemoteSubscriber' "$config_file")
+ARE_LINUX_NETWORK_NAMESPACES_USED=$(jq -r '.areLinuxNetworkNamespacesUsed' "$config_file")
+
 LOSS_RATE=$1
 DELAY=$2
 BANDWIDTH=$3
 NETWORK_NAMESPACE=$4
 
 if [ "$NETWORK_NAMESPACE" = "ns-rs" ]; then
-    INTERFACE="app-rs"
+    INTERFACE=$NETWORK_INTERFACE_SERVER
 elif [ "$NETWORK_NAMESPACE" = "ns-js" ]; then
-    INTERFACE="app-js"
+    INTERFACE=$NETWORK_INTERFACE_PUBLISHER
 elif [ "$NETWORK_NAMESPACE" = "ns-js-sub" ]; then
 	NETWORK_NAMESPACE="ns-rs"
-    INTERFACE="app-rs-sub"
+    INTERFACE=$NETWORK_INTERFACE_REMOTE_SUBSCRIBER
 fi
 
-# Display the configuration
-echo "Configured $INTERFACE at $NETWORK_NAMESPACE with:"
-echo " - Loss rate: $LOSS_RATE%"
-echo " - Delay: $DELAY ms"
-echo " - Bandwidth limit: $BANDWIDTH Mbit"
-sudo ip netns exec $NETWORK_NAMESPACE tc qdisc replace dev $INTERFACE root netem loss $LOSS_RATE% delay ${DELAY}ms rate ${BANDWIDTH}Mbit
+if [ $ARE_LINUX_NETWORK_NAMESPACES_USED = true ]; then
+	sudo ip netns exec $NETWORK_NAMESPACE tc qdisc replace dev $INTERFACE root netem loss $LOSS_RATE% delay ${DELAY}ms rate ${BANDWIDTH}Mbit
+	echo "Configured $INTERFACE at $NETWORK_NAMESPACE with:"
+	echo " - Loss rate: $LOSS_RATE%"
+	echo " - Delay: $DELAY ms"
+	echo " - Bandwidth limit: $BANDWIDTH Mbit"
+else
+	sudo tc qdisc replace dev $INTERFACE root netem loss $LOSS_RATE% delay ${DELAY}ms rate ${BANDWIDTH}Mbit
+	echo "Configured $INTERFACE with:"
+	echo " - Loss rate: $LOSS_RATE%"
+	echo " - Delay: $DELAY ms"
+	echo " - Bandwidth limit: $BANDWIDTH Mbit"
+fi
